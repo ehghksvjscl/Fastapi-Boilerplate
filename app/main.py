08b875f185c7599e13a640703bd36f61e75a5a04
ehghksvjscl <ehghkswodnjs@naver.com>
2022-04-20 22:26:39 +0900

    init postgres db and basic setting fastapi

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+from app import models
+
+app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    from app.database import engine
+
+    models.Base.metadata.create_all(bind=engine)
+
+
+@app.get("/")
+async def hello():
+    return b"hello World!"
