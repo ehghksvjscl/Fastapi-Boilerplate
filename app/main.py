ec69442c04ca97383d8345535fa2c48bb5b15a4a
ehghksvjscl <ehghkswodnjs@naver.com>
2022-04-21 00:31:11 +0900

    모델 설계

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI, Request
+from pydantic import HttpUrl
 
+from app.config import settings
 from app import models
 
-app = FastAPI()
+from lib.telegram import Telegram
+from lib import schema
 
+from devtools import debug
+
+app = FastAPI()
+telegram = Telegram(settings.TELEGRAM_BOT_TOKEN)
+
+# fastapi가 처음 실행 됬을때
 @app.on_event("startup")
 def on_startup():
     from app.database import engine
@@ -14,3 +23,22 @@ def on_startup():
 @app.get("/")
 async def hello():
     return b"hello World!"
+
+@app.post("/")
+async def webhook(request: Request):
+    r = await request.json()
+    debug(r)
+    r = schema.Update.parse_obj(r)
+    return "OK"
+
+@app.get("/me")
+async def get_me():
+    return await telegram.get_bot_info()
+
+@app.get("/wb")
+async def get_webhook():
+    return await telegram.get_webhook()
+
+@app.post("/wb")
+async def set_webhook(url: HttpUrl = Body(..., embed=True)):
+    return await telegram.set_webhook(url)
---
+++ lib/telegram.py
@@ -0,0 +1,35 @@
+from ctypes import Union
+from typing import Union
+
+import httpx # requests는 비동기를 지원하지 않음
+from  pydantic import SecretStr
+
+class Telegram:
+    API_HOST = 'https://api.telegram.org'
+
+    def __init__(self, token: Union[str, SecretStr]) -> None:
+        self._token = token
+        self.client = httpx.AsyncClient(base_url=self.host) # 비동기
+
+    @property
+    def token(self):
+        if isinstance(self._token, SecretStr):
+            return self._token.get_secret_value() # SecretStr mode
+        
+        return self._token # str mode
+
+    @property
+    def host(self):
+        return f"{self.API_HOST}/bot{self.token}"
+
+    async def get_bot_info(self) -> dict:
+        r = await self.client.get("getMe")
+        return r.json()
+
+    async def get_webhook(self) -> dict:
+        r = await self.client.get("getWebhookInfo")
+        return r.json()
+
+    async def set_webhook(self, url) -> dict:
+        r = await self.client.post("setWebhook", data={"url": url})
+        return r.json()
